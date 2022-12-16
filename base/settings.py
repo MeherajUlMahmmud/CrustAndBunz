4d9c92a89b26d1e0ba00fe81036c09d22f5dcb5c
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-12-16 15:08:34 +0600

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,9 +140,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Email settings
 EMAIL_HOST = 'smtp.gmail.com'
